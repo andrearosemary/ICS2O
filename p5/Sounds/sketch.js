a8f7502091aecc6fc0046b57e3eeae273317ac1d
Andrea <athom30@ocdsb.ca>
2017-04-20 12:22:51 -0400

    Update sketch.js

--- p5/Sounds/sketch.js
+++ p5/Sounds/sketch.js
@@ -2,20 +2,38 @@
 Andrea Thompson
 2017-01-20 */
 
-var osc = new p5.Oscillator(300);
+var osc;
+var playing = false;
 
 function setup() {
-  createCanvas(640, 480);
+  
+  osc = new p5.Oscillator();
+  osc.setType('sine');
+  osc.freq(240);
+  osc.amp(0);
+  osc.start();
+
+
 }
 
 function draw() {
   ellipse(200, 200, 80, 80);
   
+  function mouseClicked() {
+  if (mouseX > 0 && mouseX < width && mouseY < height && mouseY > 0) {
+    if (!playing) {
+      // ramp amplitude to 0.5 over 0.1 seconds
+      osc.amp(0.5, 0.05);
+      playing = true;}
+ else {
+      // ramp amplitude to 0 over 0.5 seconds
+      osc.amp(0, 0.5);
+      playing = false;
+      
+    }
+  }
+}   
  
-}
 
-function mouseIsPressed() {
-	osc.start();
-    osc.freq(261.63);
-	osc.stop(5);
-}
+
+
