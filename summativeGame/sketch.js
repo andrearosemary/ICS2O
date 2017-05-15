06e0ec7494f9e803fda3acf05c338c88ade42efd
Andrea <athom30@ocdsb.ca>
2017-05-15 14:08:16 -0400

    Add files via upload

--- summativeGame/sketch.js
+++ summativeGame/sketch.js
@@ -2,6 +2,8 @@
 Andrea Thompson
 2017-05-11 */
 
+var status = 0;  // 0 for showing Start button, 1 after user clicks on the button
+
 var rectSize = 100; //variable for circle diameter and rectangle width
 
 var squareY = 600; //the Y-position of the squares
@@ -13,7 +15,15 @@ var posYellow = 550; //variable for the X-position of the yellow rectangle
 
 var speed = 5; //speed the circles go down the screen
 
-	
+
+var bCircle = 100; //variable for the moving blue circles
+var gCircle = 100; //variable for the moving green circles
+var pCircle = 100; //variable for the moving pink circles
+var yCircle = 100; //variable for the moving yellow circles
+
+var score = 0; //variable to track the score of the player
+
+var startTime; //variable that stores at how many milliseconds after the program started the player started the game
 
 
 function setup() {
@@ -77,71 +87,92 @@ function draw() {
   
   text("J", posYellow + 15, 690); //Yellow J in square
   
+  textSize(30);
+  strokeWeight(1);
+  stroke(225);
+  fill(225);
+  text("Score:" + score, 800, 30); //Score board in top right corner
   
-  blueCircle();
-  greenCircle();
-  pinkCircle();
-  yellowCircle();
+  
+  
+  
+  
+  
+  
+  //variable changes to make circles move
+  bCircle = bCircle + speed;
+  gCircle = gCircle + speed;
+  pCircle = pCircle + speed;
+  yCircle = yCircle + speed;
 }
 
 
+function mouseClicked() {
+	if (status==0) {
+    fill(255, 100, 100);  //light red
+    text("Start", 10, 40);
+  } else {
+  printLine();}
+}
+
 function blueCircle() { //function for blue circles
 	
-	var bCircle = 300; //local variable for the moving blue circles
+
 	
 	noStroke();
 	fill(0, 41, 102);
-	ellipse(bCircle, 100, rectSize, rectSize); //outer circle
+	ellipse(300, bCircle, rectSize, rectSize); //outer circle
 	
 	fill(26, 117, 255);
-	ellipse(bCircle, 100, rectSize/2, rectSize/2); //middle circle
+	ellipse(300, bCircle, rectSize/2, rectSize/2); //middle circle
 	
 	fill(230, 240, 255);
-	ellipse(bCircle, 100, rectSize/4, rectSize/4); //inner circle
+	ellipse(300, bCircle, rectSize/4, rectSize/4); //inner circle
+	
 	
 
 }
 
 function greenCircle() { //function for green circles
 	
-	var gCircle = 400; //local variable for the moving green circles
+
 	
 	noStroke();
 	fill(0, 102, 41);
-	ellipse(gCircle, 100, rectSize, rectSize); //outer circle
+	ellipse(400, gCircle, rectSize, rectSize); //outer circle
 	
 	fill(26, 255, 117);
-	ellipse(gCircle, 100, rectSize/2, rectSize/2); //middle circle
+	ellipse(400, gCircle, rectSize/2, rectSize/2); //middle circle
 	
 	fill(230, 255, 240);
-	ellipse(gCircle, 100, rectSize/4, rectSize/4); //inner circle
+	ellipse(400, gCircle, rectSize/4, rectSize/4); //inner circle
 }
 
 function pinkCircle() { //function for pink circles
-	var pCircle = 500; //local variavle for the moving pink circles
+	
 	
 	noStroke();
 	fill(102, 0, 41);
-	ellipse(pCircle, 100, rectSize, rectSize); // outer circle
+	ellipse(500, pCircle, rectSize, rectSize); // outer circle
 	
 	fill(255, 26, 117);
-	ellipse(pCircle, 100, rectSize/2, rectSize/2); //middle circle
+	ellipse(500, pCircle, rectSize/2, rectSize/2); //middle circle
 	
 	fill(255, 230, 240);
-	ellipse(pCircle, 100, rectSize/4, rectSize/4); //inner circle
+	ellipse(500, pCircle, rectSize/4, rectSize/4); //inner circle
 }
 
 function yellowCircle() { //function for yellow circles
-	var yCircle = 600; //local variable for the moving yellow circles
+	
 	
 	noStroke();
 	fill(102, 82, 0);
-	ellipse(yCircle, 100, rectSize, rectSize); //outer circle
+	ellipse(600, yCircle, rectSize, rectSize); //outer circle
 	
 	fill(255, 209, 26);
-	ellipse(yCircle, 100, rectSize/2, rectSize/2); //middle circle
+	ellipse(600, yCircle, rectSize/2, rectSize/2); //middle circle
 	
 	fill(255, 250, 230);
-	ellipse(yCircle, 100, rectSize/4, rectSize/4); //inner circle
+	ellipse(600, yCircle, rectSize/4, rectSize/4); //inner circle
 
 }
